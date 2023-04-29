567c098d6b01de638c6e70b80e0868c079efc62a
Ol_Kuts <kuts.ol.vas@gmail.com>
2023-04-29 11:35:43 +0300

    register, login

--- rest/routes/users.router.js
+++ rest/routes/users.router.js
@@ -1,9 +1,12 @@
 import {Router} from 'express'
+import {UsersController} from '../controllers/users.controller.js'
 
 const router = new Router()
+const controller = new UsersController()
 
-router.get('/', (req, res) => {
-    res.send('<h1>Hello</h1>')
-})
+router.get('/current', (req, res) => controller.currentUser(req, res))
+router.post('/login', (req, res) => controller.loginUser(req, res))
+router.post('/register', (req, res) => controller.registerUser(req, res))
+
 
 export default router
---
+++ rest/services/users.service.js
@@ -0,0 +1,26 @@
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+import bcrypt from 'bcrypt'
+
+export class UsersService {
+    async getUser(email){
+        return prisma.user.findFirst({where: {email}})
+    }
+
+    async registerUser(email, password, name){
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt)
+
+        return await prisma.user.create({
+            data: {
+                email,
+                name,
+                password: hashedPassword
+            }
+        })
+    }
+
+    current(){
+        return 'current'
+    }
+}
